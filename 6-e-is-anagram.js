3bb511a35a6a9c6e47ee466eff0b2b8e530862ce
Kuatbek777 <kuatbek.mukabak@gmail.com>
2025-03-04 13:36:19 +0500

    check if string is anagram solution 2

--- 6-e-is-anagram.js
+++ 6-e-is-anagram.js
@@ -6,3 +6,28 @@
 var isAnagram = function (s, t) {
   return s.split("").sort().join("") == t.split("").sort().join("");
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram2 = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const lowercaseLetters = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(97 + i)
+  );
+
+  for (let i = 0; i < lowercaseLetters.length; i++) {
+    if (
+      (s.match(new RegExp(lowercaseLetters[i], "g")) || []).length !==
+      (t.match(new RegExp(lowercaseLetters[i], "g")) || []).length
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
